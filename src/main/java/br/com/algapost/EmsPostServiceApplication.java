dc1970ca7c0c6588ad9e30be625ea26a2b9bb4ba
Efraim Tenorio <efratec20.tenorio@gmail.com>
2025-07-02 14:39:39 -0300

    first commit

---
+++ src/main/java/br/com/algapost/EmsPostServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.algapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsPostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/algapost/EmsPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algapost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
